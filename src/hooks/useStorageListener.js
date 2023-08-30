1b299190bfdeb08fb4260ed892c80c7100716d3a
Alexander González B <alexgbcr@proton.me>
2023-08-29 23:13:36 -0600

    [UPLOAD] change useStorageListener file

--- src/hooks/useStorageListener.js
+++ src/hooks/useStorageListener.js
@@ -4,6 +4,13 @@ import { useState } from 'react';
 const useStorageListener = (sincronize) => {
   const [storageChange, setStorageChange] = useState(false);
 
+  if (typeof window !== 'undefined') {
+    window.addEventListener('storage', (e) => {
+      if (e.key === 'todos-v1') {
+        setStorageChange(true);
+      }
+    });
+  }
   window.addEventListener('storage', (change) => {
     if (change.key === 'TODOS_V1') {
       setStorageChange(true);
